cfaa5a917b3cff3d44f027b6dfa6e7c6e39b64f8
Abilhasan Samadov <7418@holbertonstudents.com>
2023-12-22 08:38:33 -0800

    udpated

--- builtins.c
+++ builtins.c
@@ -6,10 +6,24 @@ int check_exit(char **args)
 {
 	int i;
 
-	for (i = 0; args[i] != NULL; i++)
+	if (args[2])
 	{
-		if (strcmp(args[i], EXIT_CMD) == 0)
+		for (i = 0; args[i] != NULL; i++)
+		{
+			if (strcmp(args[i], EXIT_CMD) == 0)
+			{
+				exit_status = 2;
+				return (1);
+			}
+		}
+	}
+	else
+	{
+		if (strcmp(args[0], EXIT_CMD) == 0)
+		{
+			exit_status = 0;
 			return (1);
+		}
 	}
 
 	return (0);
